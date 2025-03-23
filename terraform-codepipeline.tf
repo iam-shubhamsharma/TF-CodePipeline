6cce8bdb6bd7432a56098d0ba141f71c4201f7bb
Shubham Sharma <37893416+iam-shubhamsharma@users.noreply.github.com>
2025-03-23 22:10:16 +0530

    Update terraform-codepipeline.tf

--- terraform-codepipeline.tf
+++ terraform-codepipeline.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "simplilearn_bucket" {
+resource "aws_s3_bucket" "bucket" {
 }
-resource "aws_s3_bucket" "simplilearn_bucket-2" {
+resource "aws_s3_bucket" "bucket-2" {
 } 
